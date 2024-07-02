6a016f50b38626da99977a626bf09c3bb1e36752
Fernando Ulloa <fulloamoreno@gmail.com>
2024-07-02 16:27:52 -0600

    Feat: CombinationSum solution algorithm

--- PULL_REQUEST_TEMPLATE.md
+++ PULL_REQUEST_TEMPLATE.md
@@ -1,13 +1,15 @@
 ## DESCRIPTION
 
-Nombre:
-Email:
-WhatsApp:
-Ciudad:
-Pais:
+Nombre: Fernando Ulloa Moreno
+Email: al288864@edu.uaa.mx
+WhatsApp: +52 449 128 3337
+Ciudad: Aguascalientes, Aguascalientes
+Pais: México
 
 ## ¿Como explicarias el proposito de este proyecto? (opcional)
 
+    Verificar que las personas que se postularon tengan conocimientos sobre estructuras de datos, recursividad y manejo de JS.
 
 ## ¿Cuál es tu stack tecnológico preferido? ¿Por qué? (opcional)
 
+    El front end me gusta mucho, me divierte el hecho de ir construyendo lo que el usuario final verá y ver cómo existen diferentes formas de hacer una sola cosa.
--- src/index.js
+++ src/index.js
@@ -7,19 +7,39 @@
  * @return {number[][]}
  */
 
-const combinationSumRecursive = (
-    
+const combinationSumRecursive = (candidates, remainingSum, currentCombination, finalCombinations, startFrom) => {
+  //If the remainingSum equals 0, it means that a solution was found.
+  if (remainingSum == 0) {
+    //Add a copy to the final solutions array and return it.
+    finalCombinations.push(currentCombination.slice())
+    return finalCombinations
   }
-  
-  /**
-   * Backtracking algorithm of finding all possible combination for specific sum.
-   *
-   * @param {number[]} candidates
-   * @param {number} target
-   * @return {number[][]}
-   */
+
+  for (let i = startFrom; i < candidates.length; i++) {
+    //If the candidate number its greater than the remaining sum, it means that it isnt a valid candidate, move on to the next iteration.
+    if (remainingSum < candidates[i]) {
+      continue
+    }
+    //Add the current candidate number to the current solution array.
+    currentCombination.push(candidates[i])
+    //Then, callback the recursive function with the modified values of the remaining sum, final combinations, current combination and the start point of search.
+    combinationSumRecursive(candidates, remainingSum - candidates[i], currentCombination, finalCombinations, i)
+    //Remove the last candidate number of the current solution array in order to try all the different paths available.
+    currentCombination.pop()
+  }
+}
+
+/**
+ * Backtracking algorithm of finding all possible combination for specific sum.
+ *
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
 const combinationSum = (candidates, target) => {
-    return combinationSumRecursive(candidates, target);
-  }
+  let finalCombinations = []
+  combinationSumRecursive(candidates, target, [], finalCombinations, 0);
+  return finalCombinations
+}
 
 module.exports = combinationSum;
